4f3c434396b487beb694c48906ef74721d2ac8dd
aloisdeniel <alois.deniel@outlook.com>
2025-09-02 20:11:29 +0000

    Deploying to gh-pages from @ aloisdeniel/omarchy_calculator@0b97e0e084bdf61cc11bbd3ff2acfe379c209bd1 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3752830459"
+    serviceWorkerVersion: "3166737517"
   }
 });
